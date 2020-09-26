b8bc1202806bd499ffa175a88b111d0a517165d5
Stephen Grice <stephengrice@live.com>
2020-09-26 10:32:10 -0400

    Finish main program for Quick Python 05

--- Quick_Python/05_Write_a_File/our_program.py
+++ Quick_Python/05_Write_a_File/our_program.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
+filename = input("What file would you like to write to? ")
 message = input("What do you want to write? ")
 raw_times_to_write = input("How many times should we write it? ")
+times_to_write = int(raw_times_to_write)
 
+with open(filename, 'w') as file:
+    for i in range(times_to_write):
+        print('Writing line',i)
+        file.write(message + '\n')
+    # File automatically closed at end of `with` by context manager
 
+print('Wrote %d lines to %s' % (times_to_write, filename))
