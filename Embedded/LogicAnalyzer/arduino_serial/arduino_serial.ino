5fc660588ad3292629a269fc8f56494e3a506e83
Stephen Grice <stephengrice@live.com>
2022-05-23 18:02:59 -0400

    Add docker files for docker101

---
+++ Embedded/LogicAnalyzer/arduino_serial/arduino_serial.ino
@@ -0,0 +1,8 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Hello world!");
+  delay(2000);
+}
---
+++ docker/docker101/flaskapp.py
@@ -0,0 +1,10 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+if __name__ == "__main__":
+    app.run(port=8080)
