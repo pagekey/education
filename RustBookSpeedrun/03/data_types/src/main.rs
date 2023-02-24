5f95e9ef6b7ee5c7b970ec71c372c6f7077c531c
Stephen Grice <stephengrice@live.com>
2023-02-24 15:22:54 -0500

    Add code from rust-sr-3

---
+++ RustBookSpeedrun/03/data_types/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let _guess: u8 = "42".parse().expect("Not a number");
+    let _guess2 = 255u8;
+    let guess3 = 1_0_0___________0_000;
+    println!("{}", guess3);
+    // tuples
+    let tup = (500, 6.4, 1);
+    let y = tup.1;
+    println!("value of y is {y}");
+    let a = [3; 5]; // [3, 3, 3, 3, 3]
+}
