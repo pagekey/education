6d87cae1c5bfa06c9f1be3ed6b63c7e9af1deeb8
Steve Grice <steve@pagekey.io>
2024-10-16 06:41:17 -0400

    Add a button just for fun

--- 164-nix-qt/src/main.cpp
+++ 164-nix-qt/src/main.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QPushButton button("Hello world!");
+    button.show();
+
     MainWindow w;
     w.show();
     return a.exec();
