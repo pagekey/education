c1b6ecb64f2a310b7111d853b3c04c0397c6dcc5
Steve Grice <steve@pagekey.io>
2024-12-29 17:56:00 -0500

    178: Get remote build working for Python! woooo

--- 178-Nix-Package-Python/nixos/default.nix
+++ 178-Nix-Package-Python/nixos/default.nix
@@ -6,7 +6,7 @@ let
       # Download from GitHub:
       defaultNix = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/pagekey/education/refs/heads/main/178-Nix-Package-Python/sample-app-python/default.nix";
-        sha256 = "sha256:0v8rdh4j8z4mhxxxw76hrycbjdxb30wm754q83gnrmby3v784v7m";
+        sha256 = "sha256:0mhjp9ig4g4wahkkrncpq5bc3f6bcnkg5qpa54dsyp0r3s669hbz";
       };
       # Uncomment for local testing:
       # defaultNix = builtins.path {
@@ -18,14 +18,15 @@ let
         owner = "pagekey";
         repo = "education";
         rev = "main";  # REPLACE WITH A TAG!
-        sha256 = "sha256-DYabJIZ9dOPdPC/m/Fj/wDkkLp6EndXgPkEpons8vqM=";
+        sha256 = "sha256-erg+Fs8l7CgzghXz1grrtd6jhm1Zj1SdENq3eAphSD8=";
       };
+      subdir = "178-Nix-Package-Python/sample-app-python";
       
       # Uncomment for local testing:
-      src = builtins.path {
-        path = ./..;
-      };
-      subdir = "/sample-app-python";
+      # src = builtins.path {
+      #   path = ./..;
+      # };
+      # subdir = "/sample-app-python";
     };
   in [
     sample-app-python
