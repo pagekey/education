831101f19a17776f8d3d2d3d44e1a6d4d97033a9
Steve Grice <steve@pagekey.io>
2024-12-29 13:19:22 -0500

    Reset hashes, branch to main

--- 176-Nix-Package-C/nixos/default.nix
+++ 176-Nix-Package-C/nixos/default.nix
@@ -1,10 +1,21 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  sample-app-c = 
-    pkgs.callPackage /home/steve/repos/education/176-Nix-Package-C/sample-app-c/default.nix {
-      src = /home/steve/repos/education/176-Nix-Package-C/sample-app-c/src;
-    } ;
+  sample-app-c =
+    let
+      defaultNix = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/pagekey/education/refs/heads/main/176-Nix-Package-C/sample-app-c/default.nix?abc=def";
+        sha256 = "";
+      };
+    in pkgs.callPackage defaultNix {
+      src = pkgs.fetchFromGitHub {
+        owner = "pagekey";
+        repo = "education";
+        rev = "main";  # REPLACE WITH A TAG!
+        sha256 = "";
+      };
+      subdir = "176-Nix-Package-C/sample-app-c/src";
+    };
   in [
     sample-app-c
   ]
