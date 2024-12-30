6169004c5213329575f762357225ac3ec8166b85
Steve Grice <steve@pagekey.io>
2024-12-29 20:06:10 -0500

    179: Get nodeEnv working!

--- 179-Nix-Package-Node/sample-app-node/default.nix
+++ 179-Nix-Package-Node/sample-app-node/default.nix
@@ -1,59 +1,55 @@
 { pkgs ? import <nixpkgs> {} }:
 
-# pkgs.stdenv.mkDerivation {
-#   pname = "sample-app-node";
-#   version = "1.0.0";
+let
+  nodeEnv =
+    pkgs.stdenv.mkDerivation rec {
+      pname = "sample-app-node";
+      version = "1.0.0";
 
-#   src = pkgs.lib.cleanSource ./.;
+      src = pkgs.lib.cleanSource ./.;
 
-#   buildInputs = [
-#     pkgs.nodejs_22
-#   ];
+      buildInputs = [
+        pkgs.nodejs_22
+      ];
 
-#   buildPhase = ''
-#     npm install
-#   '';
+      buildPhase = ''
+        npm install
+      '';
 
-#   installPhase = ''
-#     npm config set cache $out/npm-cache
-#     npm config set tmp $out/npm-temp
-#     mkdir -p $out/build
-#     npm pack --pack-destination $out/build --verbose
-#     ls -a
-#     pwd
-#     mkdir -p $out/dist
-#     cp -v *.tgz $out/dist
-#   '';
+      installPhase = ''
+        mkdir -p $out
+        cp -r node_modules $out/node_modules
+      '';
 
-#   meta = with pkgs.lib; {
-#     description = "An example npm package built with Nix.";
-#     license = licenses.mit;
-#   };
-# }
+      meta = with pkgs.lib; {
+        description = "An example Node environment built with Nix.";
+        license = licenses.mit;
+      };
+    };
+in
+  pkgs.buildNpmPackage rec {
+    pname = "sample-app-node-package";
+    version = "1.0.0";
 
+    src = ./.;
 
-pkgs.buildNpmPackage rec {
-  pname = "sample-app-node-package";
-  version = "1.0.0";
+    npmDepsHash = "sha256-cgfEGoEh2RDfoyNimw7Or1lqj4kG3tl4+I/mfWozd00=";
+    # npmBuildScript = "build";
+    dontNpmBuild = true;
 
-  src = ./.;
+    installPhase = ''
+      mkdir -p $out/bin $out/lib
+      cp -rv package.json $out/lib
+      cp -rv ${src}/src $out/lib
+      cp -rv ${nodeEnv}/node_modules $out/lib
 
-  npmDepsHash = "sha256-cgfEGoEh2RDfoyNimw7Or1lqj4kG3tl4+I/mfWozd00=";
-  # npmBuildScript = "build";
-  dontNpmBuild = true;
+      cat > $out/bin/${pname} << EOF
+  #!/bin/sh
+  NODE_PATH=$out/lib/node_modules ${pkgs.nodejs_22}/bin/node $out/lib/src/app.js
+  EOF
 
-  installPhase = ''
-    mkdir -p $out/bin $out/lib
-    cp -rv package.json $out/lib
-    cp -rv ${src}/src $out/lib
-
-    cat > $out/bin/${pname} << EOF
-#!/bin/sh
-${pkgs.lib.getExe pkgs.nodejs} $out/lib/src/app.js
-EOF
-
-    chmod +x $out/bin/${pname}
-  '';
-  
-  meta.mainProgram = "${pname}";
-}
+      chmod +x $out/bin/${pname}
+    '';
+    
+    meta.mainProgram = "${pname}";
+  }
