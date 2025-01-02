d53806c258470b637ab9c56247fe4e8ae2f40e2e
Steve Grice <steve@pagekey.io>
2025-01-01 20:14:48 -0500

    177: Tweaks

--- 177-Nix-Package-Rust/nixos/default.nix
+++ 177-Nix-Package-Rust/nixos/default.nix
@@ -1,21 +1,20 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  sample-app-c = 
-      let
-          defaultNix = builtins.path {
-              path = /home/steve/repos/education/176-Nix-Package-C/sample-app-c;
-          };
-      in pkgs.callPackage defaultNix {
-          src = builtins.path {
-              path = /home/steve/repos/education;
-          };
-          subdir = "176-Nix-Package-C/sample-app-c/src";
+  sample-app-rust =
+    let
+      defaultNix = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/pagekey/education/refs/heads/main/177-Nix-Package-Rust/sample-app-rust/default.nix";
+        sha256 = "sha256:024c96sg6b7w56jpixl388kz9qsxll9avpymmg6v8lkbsg4na1lx";
       };
-    sample-app-rust = 
-      pkgs.callPackage /home/steve/repos/education/177-Nix-Package-Rust/sample-app-rust { };
+    in pkgs.callPackage defaultNix {
+      src = pkgs.fetchFromGitHub {
+        owner = "pagekey";
+        repo = "education";
+        rev = "main";  # REPLACE WITH A TAG!
+        sha256 = "sha256-i4lYZj9i48eKDJ/VaATzMb1YidBaTu7iJpoZtEHuvac=";
+      } + "/177-Nix-Package-Rust/sample-app-rust";
+    };
   in [
-    sample-app-c
     sample-app-rust
   ]
-  
