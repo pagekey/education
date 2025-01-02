1180a628b3e3b46d10406c050b7fcec12a00c09d
Stephen Grice <stephengrice@live.com>
2025-01-02 05:30:43 -0500

    180: Add third-party lib to Go hello world

--- 180-Nix-Package-Go/sample-app-go/go.mod
+++ 180-Nix-Package-Go/sample-app-go/go.mod
@@ -1,3 +1,8 @@
 module sample-app-go
 
 go 1.22.6
+
+require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- 180-Nix-Package-Go/sample-app-go/main.go
+++ 180-Nix-Package-Go/sample-app-go/main.go
@@ -1,7 +1,11 @@
 package main
 
 import "fmt"
+import (
+    "github.com/sirupsen/logrus"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println("Hello, World from Go!")
+	logrus.Println("Hello from third-party package!")
 }
