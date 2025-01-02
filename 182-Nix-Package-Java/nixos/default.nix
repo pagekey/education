e6a438724ca8cac794d9a60b62ea056e6c9e21bc
Stephen Grice <stephengrice@live.com>
2025-01-02 07:10:01 -0500

    182: Get remote nixos build working
    Somehow worked on first try

--- 182-Nix-Package-Java/nixos/default.nix
+++ 182-Nix-Package-Java/nixos/default.nix
@@ -4,29 +4,29 @@ let
   sample-app-java = 
     let
       # Download from GitHub:
-    #   defaultNix = builtins.fetchurl {
-    #   url = "https://raw.githubusercontent.com/pagekey/education/refs/heads/main/182-Nix-Package-Java/sample-app-java/default.nix";
-    #     sha256 = "sha256:1084z6bdqvgk28hxbyprliwqaj7dv1wk5dw64alnk4fw5l5zyx5a";
-    #   };
-      # Uncomment for local testing:
-      defaultNix = builtins.path {
-        path = ../sample-app-java;
+      defaultNix = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/pagekey/education/refs/heads/main/182-Nix-Package-Java/sample-app-java/default.nix";
+        sha256 = "sha256:1br4cas27gcsndy9ag2m3sp05qckkciyyqxib5ysfhlav5kspj8l";
       };
+      # Uncomment for local testing:
+    #   defaultNix = builtins.path {
+    #     path = ../sample-app-java;
+    #   };
     in pkgs.callPackage defaultNix {
       # Download from GitHub:
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "pagekey";
-    #     repo = "education";
-    #     rev = "main";  # REPLACE WITH A TAG!
-    #     sha256 = "";
-    #   };
-    #   subdir = "182-Nix-Package-Java/sample-app-java/src";
+      src = pkgs.fetchFromGitHub {
+        owner = "pagekey";
+        repo = "education";
+        rev = "main";  # REPLACE WITH A TAG!
+        sha256 = "sha256-KgBYdj0eWAynI2Xpj/eualTxx6+AzPxgj+iwOHQwIKE=";
+      };
+      subdir = "182-Nix-Package-Java/sample-app-java/src";
       
       # Uncomment for local testing:
-      src = builtins.path {
-        path = ./..;
-      };
-      subdir = "/sample-app-java/src";
+    #   src = builtins.path {
+    #     path = ./..;
+    #   };
+    #   subdir = "/sample-app-java/src";
     };
   in [
     sample-app-java
