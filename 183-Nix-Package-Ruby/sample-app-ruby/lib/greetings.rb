58b8ddc30993d04bc8ef4036f9c3445878d91b47
Stephen Grice <stephengrice@live.com>
2025-01-03 05:46:46 -0500

    183: Delete all ruby stuff

--- 183-Nix-Package-Ruby/sample-app-ruby/lib/greetings.rb
+++
@@ -1,5 +0,0 @@
-module Greetings
-    def self.hello
-      "Hello from another file!"
-    end
-end
--- 183-Nix-Package-Ruby/sample-app-ruby/shell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = [ pkgs.ruby ];
-}
