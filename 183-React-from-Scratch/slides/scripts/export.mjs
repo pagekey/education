212897e4755e7cd233ffa99c0131c4e89ae04237
Stephen Grice <stephengrice@live.com>
2025-02-23 18:24:06 -0500

    183: Refactor export to run fully in parallel

--- 183-React-from-Scratch/slides/scripts/export.mjs
+++ 183-React-from-Scratch/slides/scripts/export.mjs
@@ -2,21 +2,29 @@ import fs from "fs";
 import * as puppeteer from "puppeteer";
 
 
-(async () => {    
-    // Use puppeteer to capture screenshots.
+async function capturePage(slide, click) {
+    // Capture a screenshot using Puppeteer.
+    const browser = await puppeteer.launch({
+        executablePath: process.env["PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH"],
+        headless: "new",
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(`http://localhost:8000?slide=${slide}&click=${click}`, { waitUntil: "networkidle2", timeout: 10000 });
+    await page.screenshot({ path: `export/${slide}_${click}.png` });
+    await browser.close();
+    console.log(`Completed ${slide}_${click}.png`);
+}
+
+(async () => {
     fs.mkdirSync("export", { recursive: true });
-    // let slide = 0;
-    await Promise.all([0,1,2].map(async slide => {
-        const browser = await puppeteer.launch({
-            executablePath: process.env["PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH"],
-            headless: "new",
-        });
-        console.log(`Exporting slide ${slide}`)
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1080 });
-        await page.goto(`http://localhost:8000?slide=${slide}`, { waitUntil: "networkidle2", timeout: 10000 });
-        await page.screenshot({ path: `export/${slide}.png` });
-        await browser.close();
-        console.log(`Done slide ${slide}`)
+    console.log("Exporting slides...");
+    await Promise.all([
+        { slide: 0, maxClick: 2 },
+        { slide: 1, maxClick: 2 },
+    ].map(async ({slide, maxClick}) => {
+        for (let click = 0; click <= maxClick; click++) {
+            capturePage(slide, click);
+        }
     }));
 })();
