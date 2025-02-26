b2df253b7bf64119e93f0f2d6d266e9cba828cbb
Stephen Grice <stephengrice@live.com>
2025-02-25 20:53:52 -0500

    183: Get export working perfectly

--- 183-React-from-Scratch/slides/scripts/export.mjs
+++ 183-React-from-Scratch/slides/scripts/export.mjs
@@ -2,7 +2,9 @@ import fs from "fs";
 import * as puppeteer from "puppeteer";
 
 
-async function capturePage(slide, click) {
+(async () => {
+    fs.mkdirSync("export", { recursive: true });
+    console.log("Exporting slides...");
     // Capture a screenshot using Puppeteer.
     const browser = await puppeteer.launch({
         executablePath: process.env["PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH"],
@@ -10,8 +12,9 @@ async function capturePage(slide, click) {
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto(`http://localhost:8000?slide=${slide}&click=${click}`, { waitUntil: "networkidle2", timeout: 10000 });
+    await page.goto(`http://localhost:8000`, { waitUntil: "networkidle2", timeout: 10000 });
     let divExists = false;
+    let slide, click;
     while (!divExists) {
         const queryParams = await page.evaluate(() => {
             const params = new URLSearchParams(window.location.search);
@@ -21,21 +24,8 @@ async function capturePage(slide, click) {
         click = queryParams.click ? queryParams.click : 0;
         await page.screenshot({ path: `export/${slide}_${click}.png` });
         await page.keyboard.press("ArrowRight");
-        divExists = await page.$('div.some-class') !== null;
+        divExists = await page.$('div#slide_error') !== null;
         await new Promise(resolve => setTimeout(resolve, 250));
     }
     await browser.close();
-}
-
-(async () => {
-    fs.mkdirSync("export", { recursive: true });
-    console.log("Exporting slides...");
-    await Promise.all([
-        { slide: 0, maxClick: 0 },
-        // { slide: 1, maxClick: 2 },
-    ].map(async ({slide, maxClick}) => {
-        for (let click = 0; click <= maxClick; click++) {
-            capturePage(slide, click);
-        }
-    }));
 })();
